9d3deb6b73b9f3a4ab1ce046c75a6a543aaaa56a
RajRishabh23 <101519471+RajRishabh23@users.noreply.github.com>
2024-05-27 14:57:27 +0530

    Add files via upload

---
+++ Javascript/basics.js
@@ -0,0 +1,99 @@
+// let Myage=18;
+// console.log(Myage);
+
+// const Mygender="Male";
+// console.log(Mygender);
+
+
+// const myAge = 20;
+// console.log('MyAge: ' + myAge + ', typeof MyAge: ' + typeof myAge );
+// console.log(myAge * 2);
+
+
+// const MyAge=22;
+// const now=50 - MyAge;
+
+// console.log(now);
+
+
+// let a1=20;
+// let a2=40;
+// let a=a1 + a2;
+// let b=a2/a1;
+// let c=a1*a2;
+
+// console.log(a +" "+ b + " "+ c);
+
+// console.log(a1 > a2);
+// console.log(a1 == a2);
+// console.log(a1 === a2);
+// console.log(a1 != a2);
+// console.log(a1 !== a2);
+
+// const age=13;
+// if (age > 18)
+
+//     {
+//     console.log('can vote');
+//     }
+// else 
+//     {
+//     const yearsLeft = 18 - age;
+//     console.log(`Aman can't vote for remaining ${yearsLeft} years`);
+//     }
+
+
+// const myAge = '20';
+// console.log(myAge, typeof myAge);
+// console.log(Number(myAge));
+
+// console.log(Number('Raj'));
+
+
+////////////coercion////////////////
+
+// console.log('I am ' + 23 + ' years old');
+// console.log('23' - '10' - 3);
+// console.log('23' / '2');
+
+
+// const age = Number(prompt("guess my age?"));
+// console.log(age);
+// console.log(typeof age);
+
+// if (age === 23) {
+//     console.log("Its Correct");
+// } else{
+//     console.log("Its wrong");
+// }
+
+
+// const hot= true;
+// const cold= false;
+
+// console.log(hot && cold);
+// console.log(hot || cold);
+
+
+// const name= "Rishabh";
+
+// switch(name) {
+//     case "Ridhi":
+//         console.log("I am a girl.");
+//         break;
+
+//     case "Aman":
+//     console.log("I ama a bot");
+//         break;
+
+//     case "Rishabh": 
+//     console.log("I am Raj");
+//         break;
+
+//     default:
+//         console.log("Not Found");
+// }
+
+
+const age =19;
+age >=18 ? console.log("correct") : console.log("not correct");
