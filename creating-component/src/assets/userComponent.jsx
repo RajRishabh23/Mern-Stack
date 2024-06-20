7f739a865ea830c2e78416b51d0ad434ec17ff01
RajRishabh23 <101519471+RajRishabh23@users.noreply.github.com>
2024-06-20 11:35:26 +0530

    creating components

---
+++ creating-component/src/assets/userComponent.jsx
@@ -0,0 +1,20 @@
+function Person() {
+    return (
+        <>
+            <h1>This is person component.</h1>
+        </>
+    )
+
+}
+
+function userComponent() {
+    return
+    (
+        <>
+            <Person />
+        </>
+
+    )
+}
+
+export default userComponent;
